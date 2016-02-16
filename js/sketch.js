ee27699032aaa194710c4e317cf2230fa75f4ad7
mikmargiotta <michele.margiotta@gmail.com>
2016-02-16 07:38:38 +0100

    Version 4
    Aggiunta la funzione di cancella e rigenera

--- js/sketch.js
+++ js/sketch.js
@@ -25,46 +25,17 @@
 		}
 		
 	}
-	var i = 0;
-	function intervallo(){
-		if(i == 0){
-			var a = new Bubble(random(width), height/2,elementi[i].totale,elementi[i].colore,elementi[i].nome, massimo);
-		}else{
-			var a = new Bubble(random(width), height/2,elementi[i].totale,elementi[i].colore,elementi[i].nome, massimo);
-		}
-	  bubbles.push(a);
-	  i++;
-	  if(bubbles.length == elementi.length){
+	
+	//a = calcella s = riparti
+	function keyTyped() {
+	  if (key === 'a') {
+	    bubbles = [];
+	    i = 0;
 	    clearInterval(stepbystep);
+	  } else if (key === 's') {
+	    stepbystep = setInterval(intervallo, 1000);
 	  }
-	}
-
-	/*============================================
-=            P5.JS SKETCH BUBBLES            =
-============================================*/
-	function setup(){
-		var myCanvas = createCanvas(larghezzaCanvas, altezzaCanvas);
-  		myCanvas.parent('canvas');
-  		colorMode(HSB, 360, 100, 100, 100);
-  		
-	}
-	function draw(){
-		clear();
-		for (var i = 0; i < bubbles.length; i++){
-		    bubbles[i].display();
-		    if(isHover){
-			for (var j = 0; j < bubbles.length; j++){
-			        if(j != indexHover){
-			          bubbles[j].grigio();
-			        }else{
-			          bubbles[indexHover].colore();
-			        }
-			      }
-			}else{
-			    bubbles[i].base();
-			}
-		}
-		
+	  return false; 
 	}
 	var i = 0;
 	function intervallo(){
@@ -77,5 +48,6 @@
 	  i++;
 	  if(bubbles.length == elementi.length){
 	    clearInterval(stepbystep);
+	    //i = 0;
 	  }
 	}
